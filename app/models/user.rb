2baa36ed73a946b1e20ab5b0de188015e5c80165
Kipleting <kipletingedwin4@gmail.com>
2022-10-30 23:05:11 +0300

    validate a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :incidents, foreign_key: :user_id
+    validates_presence_of :name, :username, :email, :password, :password_confirmation
     enum user_type: [:user, :admin]
 
-    # after_initialize :init
-
-    # def init
-    #   self.isAdmin = false unless isAdmin
-    # end
+   
     
     validates :username, presence: true, uniqueness: true
 end
